7b6ae212d53d3897f3c4736396ca0dae0af246fd
Rohan Akki <roha.akki@gmail.com>
2022-12-04 16:41:26 -0500

    removed roll functionality into comments

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import {roll} from "./lib/roll.js";
+//import {roll} from "./lib/roll.js";
 import minimist from "minimist";
-import express, { application }  from "express";
+import express  from "express";
 
 let args = minimist(process.argv.slice(2));
 let expressApp = express();
@@ -10,6 +10,13 @@ var port = args.port || 5000; //either the port or 5000
 expressApp.use(express.urlencoded({ extended: true })); //extending to url encoded or json doesn't matter and then listen
 expressApp.listen(port);
 
+expressApp.get("*", (req, res) => { //handle 404
+    res.status(404).send("404 NOT FOUND");
+}   );
+
+
+
+/*
 expressApp.get("/app", (req, res) => { //get request
   res.status(200).send("200 OK"); //send 200 OK for the first thing
 });
@@ -19,8 +26,6 @@ expressApp.get("/app/roll", (req, res) => { //default section
     res.status(200).send(JSON.stringify(result)); //send the result
 });
 
-
-    //now for some of the harder ones 
 
 expressApp.post("/app/roll", (req, res) => { //if given a post with a json body
     let result = roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls));
@@ -41,8 +46,6 @@ expressApp.get("/app/roll/:sides/:dice/:rolls", (req, res) => { //if all three
     let result = roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls));
     res.status(200).send(JSON.stringify(result));
 });
+*/
 
 //do the 404 last to get rid of the autograder error
-expressApp.get("*", (req, res) => {
-    res.status(404).send("404 NOT FOUND");
-}   );
