e84c4fa9fe27ebe6de57323d32b6fc9b478f986c
Lucy <lucyren@email.unc.edu>
2022-12-08 02:19:25 -0500

    clean up

--- server.js
+++ server.js
@@ -187,8 +187,8 @@ expressApp.post('/app/user/info/updatename/:username/:newusername', (req, res, n
     var stmt = db.prepare('SELECT * FROM userinfo');
     const currentUsers = stmt.all();
     for(var i in currentUsers){ 
-        if (req.params.username == currentUsers[i].username) { // check whether username exists
-            if (password == currentUsers[i].password) { // verify password
+        if (req.params.username == currentUsers[i].username) {
+            if (password == currentUsers[i].password) { 
                 var stmt = db.prepare('UPDATE userinfo SET username = ? WHERE username = ?');
                 const info = stmt.run(newusername, req.params.username);
                 currentUser = {
@@ -201,7 +201,7 @@ expressApp.post('/app/user/info/updatename/:username/:newusername', (req, res, n
             }
         }
     }
-    return res.status(200).send("Username not found."); // user not found
+    return res.status(200).send("Username not found.");
 })
 
 // See /app/user/delete documentation
